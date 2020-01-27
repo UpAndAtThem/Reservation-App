aeb2af12f0e8b3b5e13e0a09766415e4925e9a86
Jason Nelson <Jasonn318@gmail.com>
2020-01-27 17:26:19 -0600

    added custom directive which uses HostListeners and HostBinding to highlight table row when mouseenter and dehighlight when mouseleave

--- src/app/directives/reservation-highlight.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ReservationHighlightDirective } from './reservation-highlight.directive';
-
-describe('ReservationHighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ReservationHighlightDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/directives/reservation-highlight.directive.ts
+++ src/app/directives/reservation-highlight.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener, ElementRef, Renderer2, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appReservationHighlight]'
 })
-export class ReservationHighlightDirective {
+export class ReservationHighlightDirective implements OnInit{
+  @HostBinding('style.background') background: string;
 
-  constructor() { }
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+  ngOnInit() {
+  }
 
+  @HostListener('mouseenter') mouseOver() {
+    this.background = 'rgb(255, 255, 255)';
+  }
+
+  @HostListener('mouseleave') mouseLeave() {
+    this.background = '';
+  }
 }
